f1f5957e7b2cab3f6a1cc27b962b5f439acfaf6b
Thomas H <85164423+Siaxo@users.noreply.github.com>
2022-08-31 10:43:23 +0200

    Added some data classes

---
+++ ECommerceSite/Data/ECommerceDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ECommerceSite.Data
+{
+    public class ECommerceDBContext : DbContext
+    {
+        public ECommerceDBContext()
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
---
+++ ECommerceSite/Data/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace ECommerceSite.Data
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int EanCode { get; set; }
+        public string Category { get; set; }
+    }
+}
