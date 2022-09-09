3cc040b1c6a80a4e8ec5dc7dcf7231c73901fa3e
Thomas H <85164423+Siaxo@users.noreply.github.com>
2022-09-09 21:10:06 +0200

    Added Category name to product page

--- ECommerceSite/Pages/Product.cshtml.cs
+++ ECommerceSite/Pages/Product.cshtml.cs
@@ -15,22 +15,30 @@ namespace ECommerceSite.Pages
         }
 
         public int Id { get; set; }
-
         public string Name { get; set; }
-
         public decimal? UnitPrice { get; set; }
+        public string CategoryName { get; set; }
+        public int? CategoryId { get; set; }
 
 
 
 
         public void OnGet(int id)
         {
-            var product = _dbContext.Products.First(x => x.ProductId == id);
+            var product = _dbContext.Products.FirstOrDefault(x => x.ProductId == id);
+            var category = _dbContext.Categories.FirstOrDefault(x => x.CategoryId == product.CategoryId);
 
             Id = product.ProductId;
             Name = product.ProductName;
             UnitPrice = product.UnitPrice;
+            CategoryId = product.CategoryId;
+            CategoryName = category.CategoryName;
+
             
+            
+
+
+
 
         }
     }
